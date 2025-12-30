fc4d3e823be0ef692b812ff08b559647af23a79a
khaisendefo <khaisendefo@gmail.com>
2025-12-30 12:15:10 +0300

    add audio button and image aroken

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 new Snow ();
+
+
+const btn = document.querySelector(".button-audio");
+const music = document.getElementById("bgMusic");
+
+music.volume = 0.4;
+
+btn.addEventListener("click", () => {
+  if (music.paused) {
+    music.play();
+    btn.classList.add("active");
+  } else {
+    music.pause();
+    btn.classList.remove("active");
+  }
+});
